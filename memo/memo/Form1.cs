3ab800e48a432d01206ccdd94aaea0063fd267a7
Majli <mjandgabe@gmail.com>
2019-09-05 14:31:34 +0200

    new file:   memo/.vs/memo/v15/.suo 	new file:   memo/.vs/memo/v15/Server/sqlite3/db.lock 	new file:   memo/.vs/memo/v15/Server/sqlite3/storage.ide 	new file:   memo/.vs/memo/v15/Server/sqlite3/storage.ide-shm 	new file:   memo/.vs/memo/v15/Server/sqlite3/storage.ide-wal 	new file:   memo/memo.sln 	new file:   memo/memo/App.config 	new file:   memo/memo/Form1.Designer.cs 	new file:   memo/memo/Form1.cs 	new file:   memo/memo/Form1.resx 	new file:   memo/memo/Program.cs 	new file:   memo/memo/Properties/AssemblyInfo.cs 	new file:   memo/memo/Properties/Resources.Designer.cs 	new file:   memo/memo/Properties/Resources.resx 	new file:   memo/memo/Properties/Settings.Designer.cs 	new file:   memo/memo/Properties/Settings.settings 	new file:   memo/memo/bin/Debug/memo.exe 	new file:   memo/memo/bin/Debug/memo.exe.config 	new file:   memo/memo/bin/Debug/memo.pdb 	new file:   memo/memo/memo.csproj 	new file:   memo/memo/obj/Debug/DesignTimeResolveAssemblyReferencesInput.cache 	new file:   memo/memo/obj/Debug/TemporaryGeneratedFile_036C0B5B-1481-4323-8D20-8F5ADCB23D92.cs 	new file:   memo/memo/obj/Debug/TemporaryGeneratedFile_5937a670-0e60-4077-877b-f7221da3dda1.cs 	new file:   memo/memo/obj/Debug/TemporaryGeneratedFile_E7A71F73-0F8D-4B9B-B56E-8E70B10BC5D3.cs 	new file:   memo/memo/obj/Debug/memo.Form1.resources 	new file:   memo/memo/obj/Debug/memo.Properties.Resources.resources 	new file:   memo/memo/obj/Debug/memo.csproj.CoreCompileInputs.cache 	new file:   memo/memo/obj/Debug/memo.csproj.FileListAbsolute.txt 	new file:   memo/memo/obj/Debug/memo.csproj.GenerateResource.cache 	new file:   memo/memo/obj/Debug/memo.csprojAssemblyReference.cache 	new file:   memo/memo/obj/Debug/memo.exe 	new file:   memo/memo/obj/Debug/memo.pdb

---
+++ memo/memo/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace memo
+{
+    public partial class Form1 : Form
+    {
+
+        Random random = new Random();
+        List<string> icons = new List<string>()
+        {
+            "!","!","N","N",",",",","k","k",
+            "b","b","v","v","w","w","z","z"
+        };
+
+        Label firstClick, secondClick;
+        int clickCounter = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            AssignIconsToSquares();
+        }
+
+        private void Label_UnderScore(object sender, EventArgs e)
+        {
+            if (firstClick != null && secondClick != null)
+                return;
+
+            Label clickedLabel = sender as Label;
+            clickCounter++;
+
+            if (clickedLabel == null)
+                return;
+
+            if (clickedLabel.ForeColor == Color.Black)
+                return;
+
+            if(firstClick == null)
+            {
+                firstClick = clickedLabel;
+                firstClick.ForeColor = Color.Black;
+                return;
+            }
+
+            secondClick = clickedLabel;
+            secondClick.ForeColor = Color.Black;
+
+            CheckForWinner();
+
+            if(firstClick.Text == secondClick.Text)
+            {
+                firstClick = null;
+                secondClick = null;
+            }
+            else
+            timer1.Start();
+        }
+
+        private void CheckForWinner()
+        {
+            Label label;
+
+            for (int i = 0; i < tableLayoutPanel1.Controls.Count; i++)
+            {
+                label = tableLayoutPanel1.Controls[i] as Label;
+
+                if (label != null && label.ForeColor == label.BackColor)
+                    return;
+            }
+
+            MessageBox.Show("You Win! You need " + clickCounter + " clicks to end.");
+            Close();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+
+            firstClick.ForeColor = firstClick.BackColor;
+            secondClick.ForeColor = secondClick.BackColor;
+
+            firstClick = null;
+            secondClick = null;
+        }
+
+        private void AssignIconsToSquares()
+        {
+            Label label;
+            int randomNumber;
+
+            for (int i = 0; i < tableLayoutPanel1.Controls.Count; i++)
+            {
+                if (tableLayoutPanel1.Controls[i] is Label)
+                    label = (Label)tableLayoutPanel1.Controls[i];
+                else
+                    continue;
+                randomNumber = random.Next(0, icons.Count);
+                label.Text = icons[randomNumber];
+
+                icons.RemoveAt(randomNumber);
+            }
+
+        }
+    }
+}
